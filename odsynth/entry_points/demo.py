e71e3051e194558a44c689b936443ba334ac4609
Kofi Baafi <kbaafi@gmail.com>
2024-06-30 02:10:00 +0300

    Rename main.py to entry_points/demo.py

---
+++ odsynth/entry_points/demo.py
@@ -0,0 +1,36 @@
+import typer
+from typing_extensions import Annotated
+
+from odsynth.data_generator import DataGenerator
+from odsynth.transformers import JsonTransformer, PandasDataframeTransformer
+from odsynth.utils import load_yaml
+
+app = typer.Typer()
+
+
+@app.command()
+def main(
+    schema_spec: Annotated[
+        str, typer.Option(help="Location of schema definition for data generation")
+    ],
+    num_samples: Annotated[
+        int,
+        typer.Option(help="Number of samples to be generated")
+    ],
+    plugins_dir: Annotated[
+        str, typer.Option(help="Location for user added data generation providers.")
+    ] = None,
+):
+    schema = load_yaml(schema_spec)
+    generator = DataGenerator(
+        schema=schema,
+        plugins_dir=plugins_dir,
+        num_examples=num_samples,
+        transformer=JsonTransformer()
+    )
+    generated_data = generator.generate_data()
+    print(generated_data)
+
+
+if __name__ == "__main__":
+    app()
